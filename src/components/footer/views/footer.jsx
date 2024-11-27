379afdcf73fccb35c0635723881d537882e0b423
kando-o <64841289+kando-o@users.noreply.github.com>
2024-11-27 14:24:38 +0100

    P-13 Add Component Footer

---
+++ src/components/footer/views/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../assets/footer.css"
+
+export default function footer() {
+	
+	return (
+		<>
+			<footer className="footer">
+				<p className="footer-text">Copyright 2020 Argent Bank</p>
+			</footer>
+		</>
+	)
+}
