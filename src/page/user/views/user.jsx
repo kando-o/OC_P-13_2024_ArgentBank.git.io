b33810c67d3857bc3c1164f330648fab62f02f52
kando-o <64841289+kando-o@users.noreply.github.com>
2024-11-27 14:16:30 +0100

    P-13 Configuration display hidden & name firstName for title

--- src/page/user/views/user.jsx
+++ src/page/user/views/user.jsx
@@ -1,71 +1,109 @@
+import { useState } from "react"
 import "../assets/styles/user.css"
-import { NavLink } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { setName, resetName } from "../../../features/changeName"
 
 function User () {
+	
+	const [isHidden, setIsHidden] = useState(false)
+	const dispatch = useDispatch()
+	const name = useSelector(state => state.changeName)
+	const firstName = document.querySelector('.edit-firstName')
+	const inputeName = document.querySelector('.edit-name')
+
+	const btnHidden =  () => {
+		setIsHidden(true)
+		if (isHidden) {
+			setIsHidden(false)
+		}
+	}	
 
     return (
-        <div className="main">
-        <nav className="main-nav">
-            <NavLink className="main-nav-logo" to='/'>
-                <img
-                    className="main-nav-logo-image"
-                    src="./public/assets/img/argentBankLogo.png"
-                    alt="Argent Bank Logo"
-                />
-                <h1 className="sr-only">Argent Bank</h1>
-            </NavLink>
-            <div>
-                <a className="main-nav-item" href="/user">
-                <i className="fa fa-user-circle"></i>
-                Tony
-                </a>
-                <a className="main-nav-item" href="./index.html">
-                <i className="fa fa-sign-out"></i>
-                Sign Out
-                </a>
-            </div>
-        </nav>
-        <main className="main bg-dark">
-        <div className="header">
-            <h1>Welcome back<br />Tony Jarvis!</h1>
-            <button className="edit-button">Edit Name</button>
-        </div>
-        <h2 className="sr-only">Accounts</h2>
-        <section className="account">
-            <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-            <p className="account-amount">$2,082.79</p>
-            <p className="account-amount-description">Available Balance</p>
-            </div>
-            <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-            </div>
-        </section>
-        <section className="account">
-            <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-            <p className="account-amount">$10,928.42</p>
-            <p className="account-amount-description">Available Balance</p>
-            </div>
-            <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-            </div>
-        </section>
-        <section className="account">
-            <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-            <p className="account-amount">$184.30</p>
-            <p className="account-amount-description">Current Balance</p>
-            </div>
-            <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-            </div>
-        </section>
-        </main>
-        <footer className="footer">
-        <p className="footer-text">Copyright 2020 Argent Bank</p>
-        </footer>
-    </div>
+        <div className="main" style={{ fontFamily: 'Nunito, sans-serif' }}>
+			<main className="main bg-dark">
+				<div className="header">
+
+					<h1>Welcome back  {firstName ? firstName.value +" "+ inputeName.value : "" || inputeName ? name.value : ""}</h1>
+						<button 
+							className={`edit-button ${!isHidden ? '' : "hidden" }`}
+							onClick={() => {
+								btnHidden() 
+								dispatch(setName())
+							}}
+						>
+							Edit Name
+						</button>
+					<div className={`edit ${isHidden ? 'noHidden' : "hidden" }`}>
+						<div className={`edit-nameFirstName`}>
+							<input className="edit-firstName" 
+								type="text" 
+								placeholder="firstName" 
+							/>
+
+							<input className="edit-name" 
+								type="text"  
+								placeholder="Name" 
+							/>
+						</div>
+						<div className={`edit-validation`}>
+							<button className="edit-save" 
+							type="button"
+							onClick={() => {
+									if (firstName !== "" ) {
+										dispatch(setName(firstName.value + inputeName.value))
+										btnHidden()
+									}
+								}
+							} 
+							>
+								Save
+							</button>
+
+							<button className={`edit-cancel`} 
+								type="button"
+								onClick={() => {
+										btnHidden()
+										resteName()
+									}}
+							>
+								Cancel
+							</button>
+						</div>
+					</div>
+				</div>
+				<h2 className="sr-only">Accounts</h2>
+				<section className="account">
+					<div className="account-content-wrapper">
+						<h3 className="account-title">Argent Bank Checking (x8349)</h3>
+						<p className="account-amount">$2,082.79</p>
+						<p className="account-amount-description">Available Balance</p>
+					</div>
+					<div className="account-content-wrapper cta">
+						<button className="transaction-button">View transactions</button>
+					</div>
+				</section>
+				<section className="account">
+					<div className="account-content-wrapper">
+						<h3 className="account-title">Argent Bank Savings (x6712)</h3>
+						<p className="account-amount">$10,928.42</p>
+						<p className="account-amount-description">Available Balance</p>
+					</div>
+					<div className="account-content-wrapper cta">
+						<button className="transaction-button">View transactions</button>
+					</div>
+				</section>
+				<section className="account">
+					<div className="account-content-wrapper">
+						<h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
+						<p className="account-amount">$184.30</p>
+						<p className="account-amount-description">Current Balance</p>
+					</div>
+					<div className="account-content-wrapper cta">
+						<button className="transaction-button">View transactions</button>
+					</div>
+				</section>
+			</main>
+    	</div>
     )
 }
 
