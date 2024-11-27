501587e637e6fff286719f346f90b885db278e8e
kando-o <64841289+kando-o@users.noreply.github.com>
2024-11-27 14:13:49 +0100

    P-13 Change route for the layout + Add css for layout

---
+++ src/page/layout/layout.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from '../../components/header/views/header'
+import Footer from '../../components/footer/views/footer'
+import "../layout/assets/layout.css"
+
+export default function layout() {
+
+	return (
+		<>
+			<Header />
+
+				<div className='layoutContainer'>
+					<div className="layoutContainer-content">
+						<Outlet />
+					</div>
+				</div>
+			<Footer />
+
+		</>
+	)
+}
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,17 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import  "./assets/styles/router.css"
 import Home from "../page/home/views/home"
 import SignIn from "../page/sign-in/view/singIn"
 import User from "../page/user/views/user"
+import Layout from "../page/layout/layout"
 
 const Router = () => {
     return (
 
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home/>}  />
+				<Route path="/" element={<Layout />}>
+                	<Route index element={<Home/>} /> //rout par defaut de layout
                     <Route path="/sign-in" element={<SignIn/>} />
                     <Route path="/user" element={<User/>} />
+				</Route >
             </Routes>
         </BrowserRouter>
     )
