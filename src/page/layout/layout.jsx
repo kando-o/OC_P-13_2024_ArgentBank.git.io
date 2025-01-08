ab1a883d8b10bc3438c4381641977e050abf7184
kando-o <64841289+kando-o@users.noreply.github.com>
2025-01-08 16:08:46 +0100

    P-13 Add Layout final

--- src/page/layout/layout.jsx
+++ src/page/layout/layout.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../../components/header/views/header'
 import Footer from '../../components/footer/views/footer'
 import "../layout/assets/layout.css"
 
 export default function layout() {
-
+	
 	return (
 		<>
 			<Header />
