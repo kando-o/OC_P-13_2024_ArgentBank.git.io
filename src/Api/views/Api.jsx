64e15840aa471c5f418ed367c19a69cc2199a0b5
kando-o <64841289+kando-o@users.noreply.github.com>
2025-01-08 16:10:35 +0100

    P-13 Add STORE => transaction & API

--- src/Api/views/Api.jsx
+++ src/Api/views/Api.jsx
@@ -1,38 +1,22 @@
 import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Api() {
 	const [users, setUsers] = useState([])
 
 	// Récuperer les users
 	useEffect(() => {
+		// Au chargement récupé les utilisateurs depuis le Back
 		axios.get('http://localhost:3001/api/v1/user')
-		.then(res => setUsers(res.data))
-		.catch(err => console.log('Error lors de la recupération des users! Message error:', err))
-	},[] )
+			.then(res => setUsers(res.data))
+			.catch(err => console.log('Une erreur c\'est produit lors de la récupération des users ! Message error :', err ))
+	},[])
 
 	// Ajouter un user
-	const addUSer = () => {
-		axios.post('http://localhost:3001/api/v1/user', {email: 'luv.k@live.fr', firstName : 'kyssii', lastName : "luv " })
-		.then(res => setUsers([...users, res.data]))
-		.catch(err => console.log('Une erreur c\'est produit lors de l\'ajout de l\'user ! Message error :', err ))
+	const addUser = async () => {
+		// Ajout des user dans Le Back
+		axios.post('http://localhost:3001/api/v1/user/signup', {email: 'Kyssii.luvk@live.fr', firstName : 'kyssii', lastName : "luv", password: '890lovecharger' })
+			.then(res => setUsers([...users, res.data]))
+			.catch(err => console.log('Une erreur c\'est produit lors de l\'ajout de l\'user ! Message error :', err ))
 	}
-
-  return (
-	<div>
-		<h1>Liste des Utilisateurs</h1>
-		<ul>
-			{users.map((user, index) => (
-				<li key={index}> {user.firstName} - {user.lastName} - {user.email}</li>
-			))}
-		</ul>
-		
-		<button 
-		 type="button"
-		 onClick={addUSer}
-		> 
-			Ajouter Luv
-		</button>
-	</div>
-  )
 }
--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit"
 import  changeName  from "./features/changeName"
+import transaction from "./features/transaction"
 
 export const store = configureStore ({
 
 	reducer : {
-		changeName
+		changeName,
+		transaction
 	}
 })
