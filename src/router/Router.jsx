10508cfd80fc9e95c2d27ca8cad4efc7853169c2
kando-o <64841289+kando-o@users.noreply.github.com>
2025-01-08 16:14:43 +0100

    P-13 Add componant FOOTER FINAL & REMOV css rooter

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,6 +3,7 @@ import Home from "../page/home/views/home"
 import SignIn from "../page/sign-in/view/singIn"
 import User from "../page/user/views/user"
 import Layout from "../page/layout/layout"
+import Transaction from "../page/transaction/views/transaction"
 
 const Router = () => {
     return (
@@ -10,9 +11,10 @@ const Router = () => {
         <BrowserRouter>
             <Routes>
 				<Route path="/" element={<Layout />}>
-                	<Route index element={<Home/>} /> //rout par defaut de layout
+                	<Route index element={<Home/>} /> //route par defaut de layout
                     <Route path="/sign-in" element={<SignIn/>} />
                     <Route path="/user" element={<User/>} />
+                    <Route path="/user/transaction" element={<Transaction/>} />
 				</Route >
             </Routes>
         </BrowserRouter>
