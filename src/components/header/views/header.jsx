62ffdc427c3a48047803a5e6507ba28853d4136b
kando-o <64841289+kando-o@users.noreply.github.com>
2025-01-08 16:11:28 +0100

    P-13 Add HEADER FINAL

--- src/components/header/views/header.jsx
+++ src/components/header/views/header.jsx
@@ -1,10 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink, useLocation } from "react-router-dom"
 import "../assets/header.css"
-import { useDispatch } from 'react-redux'
+import axios from "axios"
 
-export default function header() {
+export default function Header() {
 	const location = useLocation()	
+	
+	const [nameProfil, setNameProfil] = useState("")
+	
+	// Récuperer les users avec le bon profil
+	useEffect(() => {
+		// Si le token existe 
+		if (localStorage.getItem("token")) {
+			const tokenStr = localStorage.getItem("token")
+			axios.post('http://localhost:3001/api/v1/user/profile', {}, { headers: {"Authorization" : `Bearer ${tokenStr}`}}) // Fait une requête vers l'API vérifie la validité du token pour autorisé la récupération du profil
+				.then(res => {
+					setNameProfil(res.data.body.firstName)					
+				})
+				.catch(err => console.log('Une erreur c\'est produit lors de la récupération de l\'user ! Message error :', err ))
+		}
+	}, [])		
 
 	return (
 		<>
@@ -12,15 +27,43 @@ export default function header() {
                 <NavLink className="nav-logo" to='/'>
                     <img
                         className="nav-logo-image"
-                        src="./public/assets/img/argentBankLogo.png"
+                        src="/public/assets/img/argentBankLogo.png"
                         alt="Argent Bank Logo"
                     />
                     <h1 className="sr-only">Argent Bank</h1>
                 </NavLink>
-                <div>
+
+                <div className='nav-link'>
+					{/* Ne pas oublier les parenthèse pour la priorité des opérateurs */}
+					{
+						<i className="fa fa-user-circle"></i> // Logo profil
+					} 
+					{
+						nameProfil &&
+						(
+							location.pathname === "/user" || 
+							location.pathname === "/user/transaction"
+						) 
+						&& 
+						<p>
+							{nameProfil}
+						</p>
+					}
+					{
+						(
+							location.pathname === "/user" || 
+							location.pathname === "/user/transaction"
+						)
+						&& 
+						<i className="fa fa-sign-out outColor"></i>
+					}
+
                     <a className="nav-item" href="/sign-in">
-                    <i className="fa fa-user-circle"></i>
-                    {location.pathname === "/user" ? "Sign Out" : "Sign in"}
+					{
+						location.pathname === "/user" || 
+						location.pathname === "/user/transaction" ?
+						"Sign Out" : "Sign in"
+					}
                     </a>
                 </div>
             </nav>
