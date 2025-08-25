c523bf6d3ad4163f4fdeaf921df05de10ecd1b38
kando-o <64841289+kando-o@users.noreply.github.com>
2025-08-25 16:02:39 +0200

    P-13 Add DOC Swagger & remove draw

--- README.md
+++ README.md
@@ -36,6 +36,10 @@ Run Sass for the CSS
 # React + Vite
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
+## API Documentation
+
+To learn more about how the API works, once you have started your local environment, you can visit: http://localhost:5173/docs.html#/Transactions/get_api_v1_transactions
+
 Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
--- src/page/sign-in/view/singIn.jsx
+++ src/page/sign-in/view/singIn.jsx
@@ -17,61 +17,61 @@ function SignIn() {
 	const navigate = useNavigate()
 
 	// Au clique sur le btn Sign In
-    const handleSignIn = async (ev) => {
-			ev.stopPropagation()
-			ev.preventDefault()
-			//Lance une requête post à l'API avec en paramètre * email & passeword *
-        axios.post('http://localhost:3001/api/v1/user/login', {email: form.username, password: form.password})
-            .then(res => {
-                console.log("User logged in successfully");
-								dispatch(login({
-									token: res.data.body.token,
-									rememberme: form.rememberme
-								}))
-                navigate("/user");
-            })
-            .catch(err => {
-							console.log("User not logged in", err);
-							setFailed("Mot de passe ou Email incorrect")
-            })
-    }
+	const handleSignIn = async (ev) => {
+		ev.stopPropagation()
+		ev.preventDefault()
+		//Lance une requête post à l'API avec en paramètre * email & passeword *
+			axios.post('http://localhost:3001/api/v1/user/login', {email: form.username, password: form.password})
+					.then(res => {
+							console.log("User logged in successfully");
+							dispatch(login({
+								token: res.data.body.token,
+								rememberme: form.rememberme
+							}))
+							navigate("/user");
+					})
+					.catch(err => {
+						console.log("User not logged in", err);
+						setFailed("Mot de passe ou Email incorrect")
+					})
+	}
 
-		const handleChange = (e) => {
-			setForm(c => ({
-				...c,
-				[e.target.name]: e.target.value
-			}))
-			// à chaque changement copie le contenue de l'ancien form remplace la valeur du champ name par la modification de l'user
-		}
+	const handleChange = (e) => {
+		setForm(c => ({
+			...c,
+			[e.target.name]: e.target.value
+		}))
+		// à chaque changement copie le contenue de l'ancien form remplace la valeur du champ name par la modification de l'user
+	}
 
-    return (
-			<div className="main">
-				<section className="sign-in-content">
-					<i className="fa fa-user-circle sign-in-icon" />
-					<h1>Sign In</h1>
-					<form onSubmit={handleSignIn}>
-						<div className="input-wrapper">
-								<label>Username</label>
-								<input name="username" type="text" id="username" value={form.username} onChange={handleChange} />
-						</div>
-						<div className="input-wrapper">
-								<label>Password</label>
-								<input name="password" type="password" id="password" value={form.password} onChange={handleChange} />
-						</div>
-						<div className="input-remember">
-								<input name="rememberme" type="checkbox" id="remember-me" value={form.rememberme} onChange={handleChange} />
-								<label>Remember me</label>
-						</div>
-						<button className="sign-in-button" type="submit">
-							Sign In
-						</button>
-						<div className='sign-in-error'>
-							<p>{failed}</p>
-						</div>
-					</form>
-				</section>
-			</div>
-    );
+	return (
+		<div className="main">
+			<section className="sign-in-content">
+				<i className="fa fa-user-circle sign-in-icon" />
+				<h1>Sign In</h1>
+				<form onSubmit={handleSignIn}>
+					<div className="input-wrapper">
+							<label>Username</label>
+							<input name="username" type="text" id="username" value={form.username} onChange={handleChange} />
+					</div>
+					<div className="input-wrapper">
+							<label>Password</label>
+							<input name="password" type="password" id="password" value={form.password} onChange={handleChange} />
+					</div>
+					<div className="input-remember">
+							<input name="rememberme" type="checkbox" id="remember-me" value={form.rememberme} onChange={handleChange} />
+							<label>Remember me</label>
+					</div>
+					<button className="sign-in-button" type="submit">
+						Sign In
+					</button>
+					<div className='sign-in-error'>
+						<p>{failed}</p>
+					</div>
+				</form>
+			</section>
+		</div>
+	);
 }
 
 export default SignIn;
